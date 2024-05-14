337d64cee2544deb0c9b116f9c8762b58a6ced52
Nicole Lopes <nicole.lopes@neuco.com.br>
2024-05-13 21:32:26 -0300

    Criação da Página Inicial

--- src/pages/MagicCards.tsx
+++ src/pages/MagicCards.tsx
@@ -1,7 +1,15 @@
-import { Typography } from "@mui/material";
+import AutoFixHighIcon from "@mui/icons-material/AutoFixHigh";
+import PageHeader from "../components/PageHeader";
+import ContentSearchCards from "../modules/magic-cards/ContentSearchCards";
 
-export default function MagicCards(){
-    return(
-        <Typography>tela inicial</Typography>
-    )
+export default function MagicCards() {
+  return (
+    <>
+      <PageHeader
+        actions={<AutoFixHighIcon sx={{ fontSize: "50px" }} />}
+        titlePage="Cartas Mágicas"
+      />
+      <ContentSearchCards />
+    </>
+  );
 }
