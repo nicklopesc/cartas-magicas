c372c70c6832e9d58783f6bed6d6657bc89ed804
Nicole Lopes <nicole.lopes@neuco.com.br>
2024-05-13 21:32:50 -0300

    Criação de Componentes para título da página

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,25 @@
+import { Stack, Typography } from "@mui/material";
+import { type ReactNode } from "react";
+
+export type PageHeaderProps = {
+  titlePage: string;
+  actions?: ReactNode;
+};
+
+export default function PageHeader({
+  titlePage,
+  actions,
+}: PageHeaderProps) {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      paddingTop="70px"
+      paddingX="30px"
+    >
+       {actions}
+      <Typography variant="h3" fontWeight="bold" marginLeft="10px">{titlePage}</Typography>
+    </Stack>
+  );
+}
