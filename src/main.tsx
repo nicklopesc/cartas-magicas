97435517bd7dcc340321a38b1f49d42dc23c97d1
Nicole Lopes <nicole.lopes@neuco.com.br>
2024-05-14 13:22:41 -0300

    Correção de erro

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { routes } from './router/routes.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from "react";
+import ReactDOM from "react-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { routes } from "./router/routes.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter(routes);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
-     <QueryClientProvider client={queryClient}>
-       <RouterProvider router={router} />
-     </QueryClientProvider>
-   
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
