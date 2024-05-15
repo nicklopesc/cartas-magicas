1ddc43583fe1c352b4a4a8a40d6304a7f35b7b7b
Nicole Lopes <nicole.lopes@neuco.com.br>
2024-05-14 21:43:06 -0300

    Utilização do tanstack react query

---
+++ src/modules/cards-collection/hooks/useCollectionData.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../../../service/api/api-instance";
+
+interface CollectionData {
+  code: string;
+  cards: {
+    imageUrl: string;
+    name: string;
+    manaCost: string;
+    colorIdentity: string;
+    text: string;
+  }[];
+}
+
+interface useCollectionDataProps {
+  code: string;
+}
+
+export function useCollectionData(code: string) {
+  // console.log("CODEE", code);
+  const fetchCollectionData = async ({ code }: useCollectionDataProps) => {
+    try {
+      const response = await api.get(`/sets/${code}/booster`);
+      console.log("RESP", response);
+      return response.data as CollectionData;
+    } catch (error) {
+      throw new Error("Erro ao buscar dados da coleção.");
+    }
+  };
+
+  const query = useQuery({
+    queryKey: ["collection-data", code],
+    queryFn: () => fetchCollectionData({ code }),
+  });
+
+  return query;
+}
